ffd087b264527946ebf575f8d3499d50f854fd70
Talha Akyildiz <talhaa@outlook.com>
2014-01-12 17:46:07 +0200

    First Commit
    Beginning of python tutorial

---
+++ tutorial.py
@@ -0,0 +1,17 @@
+name = "talha"
+5+5
+y = 25
+print(5 ** 2)
+x = 5
+5 / 3
+5 // 3
+y
+city_numbers ={ 'Konya' : 332 , 'Ankara' : 312 , 'Istanbul' : (212 , 216) }
+type(city_numbers.keys())
+print(city_numbers.keys())
+print(city_numbers.values())
+print(city_numbers.items())
+city_numbers.has_key('Istanbul')
+
+
+
