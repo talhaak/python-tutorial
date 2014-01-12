1d18796a38ba080ab807e017ff65229f36b8ac5c
Talha Akyildiz <talhaa@outlook.com>
2014-01-12 18:21:43 +0200

    applying del function

--- tutorial.py
+++ tutorial.py
@@ -5,12 +5,13 @@ print(5 ** 2)
 x = 5
 5 / 3
 5 // 3
-y
+
 city_numbers ={ 'Konya' : 332 , 'Ankara' : 312 , 'Istanbul' : (212 , 216) }
 type(city_numbers.keys())
 print(city_numbers.keys())
 print(city_numbers.values())
 print(city_numbers.items())
+del city_numbers['Konya']
 
 
 
